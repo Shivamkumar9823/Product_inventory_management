cb05e17494b2b666bfaa691ca1433087ed996908
Shivam kumar <135254798+Shivamkumar9823@users.noreply.github.com>
2026-01-26 23:56:43 +0530

    Delete backend directory

--- backend/models/product.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Product struct {
-	ID       int     `db:"id" json:"id"`
-	Name     string  `db:"name" json:"name"`
-	Category string  `db:"category" json:"category"`
-	Price    float64 `db:"price" json:"price"`
-	Quantity int     `db:"quantity" json:"quantity"`
-}
--- backend/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	ID       int    `db:"id" json:"id"`
-	Name     string `db:"name" json:"name"`
-	Email    string `db:"email" json:"email"`
-	Password string `db:"password" json:"password"`
-}
